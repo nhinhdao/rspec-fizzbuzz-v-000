4e6dd5cb33942c234b48db644077ec34388b473c
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-14 02:26:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@
 def fizzbuzz(num)
       if (num % 15 == 0)
              "FizzBuzz"
-      elsif (num % 5 == 0)
+      elsif (num % 5 == 0 && num % 15!= 0)
             puts "Buzz"
-      elsif (num % 3 == 0)
+      elsif (num % 3 == 0 && num % 15!= 0)
             puts "Fizz"
       else
             puts "nil"
