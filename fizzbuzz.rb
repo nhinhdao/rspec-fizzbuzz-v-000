2a788b5e91989d4254d02c10796a948d5f1ba4e8
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-14 02:23:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(num)
-      if (num % 15 == 0)
-             "FizzBuzz"
-      # elsif (num % 5 == 0)
-      #       puts "Buzz"
-      # elsif (num % 3 == 0)
-      #       puts "Fizz"
-      # else
-      #       puts "nil"
-end
+# def fizzbuzz(num)
+#       if (num % 15 == 0)
+#              "FizzBuzz"
+#       # elsif (num % 5 == 0)
+#       #       puts "Buzz"
+#       # elsif (num % 3 == 0)
+#       #       puts "Fizz"
+#       # else
+#       #       puts "nil"
+# end
 
 # fizzbuzz(3)
 # fizzbuzz(25)
